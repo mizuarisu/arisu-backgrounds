aee98c65cefe0cc3f5c503060eb6738c70dc3a20
mizuarisu <arisushinetheday@gmail.com>
2026-05-20 19:18:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,9 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets:['latin'] })
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-  display: 'swap',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-  display: 'swap',
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
 })
 
 export const metadata: Metadata = {
@@ -25,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} ${geistSans.variable} ${geistMono.variable}`}>
+      <body className={inter.variable}`}>
         {children}
       </body>
     </html>
